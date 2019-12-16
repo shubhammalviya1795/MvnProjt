73794c184855b5d0305d68fb891d270a4905460c
Shubham <Shubham@DESKTOP-B3I5BBO>
2019-12-16 21:34:03 +0530

    first Maven git project add

---
+++ gitProjt/src/test/java/com/qa/login/LoginTest.java
@@ -0,0 +1,16 @@
+package com.qa.login;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	@Test
+	public void Loginmtd() {
+		System.out.println("shubhProj.....");
+	}
+
+	@Test
+	public void Homemtd() {
+		System.out.println("Hometset.....");
+	}
+	
+}
